26d4912a618a96c5bc15f846a1321de871400f82
shivank.mishra <shivank.mishra@tothenew.com>
2021-02-08 01:44:41 +0530

    Two-way data-binding using JS

--- src/README.txt
+++ src/README.txt
@@ -1,22 +1,3 @@
-This is a simple POC for Gulp use.
+This is a simple POC for Two-way-data-binding use.
 
-- Use gulp serve to run the application in browser.
-
-Globbing -
-**/*.filetype
-
-Gulp-useref
-It concatenates any number of CSS and JavaScript files into a single file by looking for a comment that starts with
-“<!–build: 'path to where file has to be stored ” and ends with “<!–endbuild–>”
-
-
-Gulp-uglify - uglify()
-This plugin helps with minifying JavaScript files.
-
-Gulp-cssnano - cssnano()
-This plugin helps with minifying JavaScript files.
-
-Gulp-imagemin - imagemin()
-It helps to optimize images and reducing size.
-
-Gulp-cache - cache()
+- Use "gulp serve" to run the application in browser.
--- src/app/app.js
+++ src/app/app.js
@@ -1,5 +1,36 @@
-var app1 = angular.module('app1',[]);
-
-app1.controller('ctrl1',function($scope){
-    $scope.randomNum= Math.floor((Math.random()*10)+1);
-});
+(function () {
+    var dataitems = document.querySelectorAll('[two-way-data-bind]'),
+        scope = {};
+    dataitems.forEach(function (ele) {
+        if(ele.type === 'text') {
+            var bindData = ele.getAttribute('two-way-data-bind');
+            addBindScope(bindData);
+            ele.onkeyup = function () {
+                scope[bindData] = ele.value;
+            }
+        }
+        function addBindScope(items) {
+            if(!scope.hasOwnProperty(items)){
+                var value;
+                Object.defineProperty(scope,items, {
+                    set: function (new_value) {
+                        dataitems.forEach(function (ele) {
+                            if (ele.getAttribute('two-way-data-bind') === items) {
+                                if(ele.type === 'text') {
+                                    ele.value = new_value;
+                                }
+                                else if(!ele.type) {
+                                    ele.innerHTML = new_value;
+                                }
+                            }
+                        });
+                    },
+                    get: function () {
+                        return value;
+                    },
+                    enumerable: true
+                })
+            }
+        }
+    });
+})();
