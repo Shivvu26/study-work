399556f43682830fce0765bae0bbae12876e8390
shivank.mishra <shivank.mishra@tothenew.com>
2021-02-10 13:11:38 +0530

    implemented dependency injection

--- src/app/app.js
+++ src/app/app.js
@@ -1,10 +1,19 @@
-import './css/styles.css';
+
 var app1 = angular.module('app1',[]);
+
+
+app1.service('exService', function(){
+    this.add = function(a,b) {
+        return a+b;
+    }
+});
+
 
 app1.controller('ctrl1', ctrl1);
 
-function ctrl1($scope) {
+function ctrl1($scope,exService) {                                  //injecting exService
     $scope.randomNum = Math.floor((Math.random() * 10) + 1);
+    $scope.DI_Result = exService.add(4,7);
 };
 
 
