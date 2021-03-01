e47274ce36dfc45fa7cfca1d2fcaa0bcbf5b7ac0
shivank.mishra <shivank.mishra@tothenew.com>
2021-03-02 00:47:59 +0530

    $http client request and data sharing b/w components using factory

--- src/app/app.js
+++ src/app/app.js
@@ -1,25 +1,47 @@
-var app1 = angular.module('app1',[]);
+var app1 = angular.module('app1', []);
 app1.controller('ctrl1', ctrl1);
 
-var myFunc = function() {
+var myFunc = function () {
     this.name = "default name";
-    this.$get = function() {
+    this.$get = function () {
         this.name = "new name"
         return "Hello from MyFunc.$get(), this.name = " + this.name;
     };
     return "Hello from MyFunc(), this.name = " + this.name;
 }
 
+app1.factory('Sharing', function() {
+    return {
+        name: ''
+    };
+});
 
-app1.service( 'myService', myFunc );
-app1.factory( 'myFactory', myFunc );
-app1.provider( 'myProv', myFunc );
+app1.service('myService', myFunc);
+app1.factory('myFactory', myFunc);
+app1.provider('myProv', myFunc);
 
-function ctrl1($scope, myService, myFactory, myProv) {
+function ctrl1($scope, myService, myFactory, myProv, $http, Sharing) {
     $scope.randomNum = Math.floor((Math.random() * 10) + 1);
     $scope.serviceOutput = "myService = " + myService;
     $scope.factoryOutput = "myFactory = " + myFactory;
     $scope.providerOutput = "myProvider = " + myProv;
+
+    $http.get('data.js')
+        .then(function (response) {
+            $scope.jsondata = response.data;
+            console.log("status:" + response.status);
+        }).catch(function (response) {
+        console.error('Error occurred:', response.status, response.data);
+    }).finally(function () {
+        console.log("Task Finished.");
+    });
+
+    $scope.data = Sharing;
 };
 
+app1.controller('ctrl2', ctrl2);
+
+function ctrl2($scope, Sharing) {
+$scope.data = Sharing;
+}
 
---
+++ src/app/data.js
@@ -0,0 +1,17 @@
+[
+    {
+        "id": "1",
+        "name": "Honey",
+        "location": "India"
+    },
+    {
+        "id": "2",
+        "name": "Bunny",
+        "location": "USA"
+    },
+    {
+        "id": "3",
+        "name": "Sunny",
+        "location": "Spain"
+    }
+]
