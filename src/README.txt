97d6a20c2641abb95ea50e95f38afc145e069098
shivank.mishra <shivank.mishra@tothenew.com>
2021-03-07 03:07:19 +0530

    Directives

--- src/README.txt
+++ src/README.txt
@@ -1,1 +1,26 @@
 Just an angular start app.
+
+Predefined Directives:
+ng-app, ng-init , ng-repeat, ng-model, ng-change, ng-bind...etc
+
+
+
+
+Custom Directives:
+
+we can invoke a directive by using
+
+Element name            <test-directive></test-directive>
+Attribute               <div test-directive></div>
+Class                   <div class="test-directive"></div>
+Comment                 <!-- directive: test-directive -->
+
+By adding a restrict property with the value "A", the directive can only be invoked by attributes.
+
+The legal restrict values are:
+
+E for Element name
+A for Attribute
+C for Class
+M for Comment
+By default the value is EA, meaning that both Element names and attribute names can invoke the directive.
--- src/app/app.js
+++ src/app/app.js
@@ -5,6 +5,52 @@ app1.controller('ctrl1', ctrl1);
 
 function ctrl1($scope) {
     $scope.randomNum = Math.floor((Math.random() * 10) + 1);
+    $scope.value = "Printing the line"
 };
+
+app1.directive('samDir', function () {
+    return {
+        templateUrl: 'dir.html',
+    }
+});
+
+app1.directive('secDir', function () {
+    return {
+        link:function($scope,element) {                         //link is used to  access events in DOM
+            element.bind('click',function () {
+                element.html('You clicked! Awesome , hi..!!, m coming from a directive here.');
+            });},
+    }
+});
+
+app1.directive('firstDirective', function () {
+    return {
+        restrict: 'E',
+        controller: function ($scope) {
+            this.data = "Initial Value";
+
+            this.change = function (val) {
+                $scope.$emit('val-change', val);
+                this.data = val;
+            }
+        },
+        controllerAs: 'first'
+    }
+});
+
+app1.directive('secondDirective', function () {
+    return {
+        restrict: 'E',
+        controller: function ($scope) {
+            var vm= this;
+            $scope.$on('val-change', function (e,data) {
+                vm.data = data;
+            });
+            this.data = "Initial Value";
+        },
+        controllerAs: 'second'
+    }
+})
+
 
 
